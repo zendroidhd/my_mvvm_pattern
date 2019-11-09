6fb982fd529d1c0ff49856afbefdd42e23dca722
Jacque Terrell <jacqueterrell@yahoo.com>
2019-11-09 12:44:02 -0800

    Added minor text updates (#21)

--- app/src/main/java/com/technologies/zenlight/earncredits/userInterface/home/myProfile/MyProfileDataModel.kt
+++ app/src/main/java/com/technologies/zenlight/earncredits/userInterface/home/myProfile/MyProfileDataModel.kt
@@ -1,5 +1,7 @@
 package com.technologies.zenlight.earncredits.userInterface.home.myProfile
 
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.technologies.zenlight.earncredits.data.AppDataManager
 import com.technologies.zenlight.earncredits.data.model.api.UserProfile
@@ -46,12 +48,40 @@ class MyProfileDataModel @Inject constructor(private val dataManager: AppDataMan
                 .set(profile)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
+                        reAuthenticateFirebase(email,password)
+                        dataManager.getSharedPrefs().userEmail = email
+                        dataManager.getSharedPrefs().userName = userName
+                        dataManager.getSharedPrefs().userPassword = password
                         viewModel.callbacks?.onProfileDataUpdated()
                     } else {
                         val message = task.exception?.message ?: "Authentication Failed (F)"
                         viewModel.callbacks?.handleError("Error", message)
                     }
                 }
+        }
+    }
+
+    private fun reAuthenticateFirebase(newEmail: String, newPasswordString: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+
+        user?.let {
+            it.updateEmail(newEmail).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i("TAG", "email updated")
+                } else {
+                    Log.e("TAG", "email failed to update")
+                }
+            }
+
+            it.updatePassword(newPasswordString)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.i("TAG", "password updated")
+                    } else {
+                        Log.e("TAG", "password failed to update")
+                    }
+                }
+
         }
     }
 }
